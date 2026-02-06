6a4452f0385a6a11091413dab11b0981d37f4186
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-02-06 08:27:24 +0500

    NEW FEATURES

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   customerName: {
     type: String,
     required: true,
